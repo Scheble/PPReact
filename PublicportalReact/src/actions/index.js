b2b559d612d7d24da5aabb732aff1bb92b3551b1
Santiago Felipe Cheble <santiago.cheble@gmail.com>
2021-07-07 12:13:05 -0300

    Delete PublicportalReact directory

--- PublicportalReact/src/actions/index.js
+++
@@ -1,5 +0,0 @@
-export const increseSize = () => {
-  return {
-    type: "INCRESE_SIZE",
-  };
-};
--- PublicportalReact/src/components/ReduxProvider.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import ReduxProvider from "./ReduxProvider";
-
-test("App should load", () => {
-  render(<ReduxProvider />);
-  expect(screen.findAllByTestId("root"));
-});
